8f2b29f3ac13e32fa2220a9efc89134681738a61
Barham Farraj <barhamfarraj@icloud.com>
2022-05-16 20:15:39 +0300

    Done

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import SnackListView, SnackDetailView
+from .views import (HomeView,
+                    SnackListView,
+                    SnackDetailView,
+                    SnackCreateView,
+                    SnackUpdateView,
+                    SnackDeleteView,
+                    FormView)
 
 urlpatterns = [
-    path('', SnackListView.as_view(), name='Snack_List'),
+    path('', HomeView.as_view(), name='Home'),
+    path('snack_list/',SnackListView.as_view(), name='snack_list'),
     path('<int:pk>', SnackDetailView.as_view(), name='snack_detail'),
+    path('create/', SnackCreateView.as_view(), name = "create_thing"),
+    path('update/<int:pk>', SnackUpdateView.as_view(), name = "update_thing"),
+    path('delete/<int:pk>', SnackDeleteView.as_view(), name = "delete_thing"),
+    path('form/', FormView.as_view(), name = "form_thing"),
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,14 +1,39 @@
 from django.shortcuts import render
-from django.views.generic import ListView,DetailView
+from django.views.generic import (TemplateView,
+                                ListView,
+                                DetailView,
+                                CreateView,
+                                UpdateView,
+                                DeleteView
+                                )
 from .models import Snack
 
-
+class HomeView(TemplateView):
+    template_name = 'home.html'
+    
 class SnackListView(ListView):
     template_name = "snack_list.html"
     model = Snack
-    context_object_name = 'snack_list'  
-         
+    context_object_name = 'snack_list'       
+
 class SnackDetailView(DetailView):
     template_name = "snack_detail.html"
     model = Snack
     
+class SnackCreateView(CreateView):
+    template_name = "snack_create.html"
+    model = Snack
+    fields = ["title","purchaser", "description"]
+
+
+class SnackUpdateView(UpdateView):
+    template_name = "snack_update.html"
+    model = Snack
+    fields = ["title", "description"]
+
+class SnackDeleteView(DeleteView):
+    template_name = "snack_delete.html"
+    model = Snack
+    success_url ='/'
+class FormView(TemplateView):
+    template_name = "form.html"
